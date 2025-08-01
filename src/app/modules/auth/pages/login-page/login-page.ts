3a2e050786194cf8696cac4af2aeb9b68ffa5c68
pcurich <curichpedro@gmail.com>
2025-08-01 09:22:50 -0500

    feat(login-page): add page of login and navigation from main menu

--- src/app/modules/auth/pages/login-page/login-page.ts
+++ src/app/modules/auth/pages/login-page/login-page.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,6 +10,45 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss'
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
+  errorSession: boolean = false
+  formLogin: UntypedFormGroup = new UntypedFormGroup({});
+
+  constructor(
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.formLogin = new UntypedFormGroup(
+      {
+        email: new UntypedFormControl('', [
+          Validators.required,
+          Validators.email
+        ]),
+        password: new UntypedFormControl('',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(12)
+          ])
+      }
+    )
+  }
+
+  sendLogin(): void {
+    const { email, password } = this.formLogin.value
+    // this.authService.sendCredentials(email, password)
+    //   //TODO: 200 <400
+    //   .subscribe(responseOk => { //TODO: Cuando el usuario credenciales Correctas ✔✔
+    //     console.log('Session iniciada correcta', responseOk);
+    //     const { tokenSession, data } = responseOk
+    //     this.cookie.set('token', tokenSession, 4, '/') //TODO:📌📌📌📌
+    //     this.router.navigate(['/', 'tracks'])
+    //   },
+    //     err => {//TODO error 400>=
+    //       this.errorSession = true
+    //       console.log('⚠⚠⚠⚠Ocurrio error con tu email o password');
+    //     })
+
+  }
 
 }
