0e8f96b30af2d673cb03e9163b133dd5492ad393
pcurich <curichpedro@gmail.com>
2025-08-06 02:31:51 -0500

    feat(routes) add dynamic track route and update routing logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { authChildGuard, authGuard } from '@modules/auth/guards/auth.guard';
+import { authGuard } from '@modules/auth/guards/auth.guard';
 import { HomePage } from '@modules/home/pages/home-page/home-page';
 
 export const routes: Routes = [
@@ -12,7 +12,6 @@ export const routes: Routes = [
     path: '',
     component: HomePage,
     canActivate: [authGuard],
-    canActivateChild: [authChildGuard],
     loadChildren: () =>
       import('./modules/home/home.routes').then(m => m.HOME_ROUTES)
   }
